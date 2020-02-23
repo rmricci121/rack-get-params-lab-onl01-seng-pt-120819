c901bba6211f0a709dcd7d364f0027ae9ff9d9ce
Robert M Ricci <rmricci121@gmail.com>
2020-02-23 10:39:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,15 +14,16 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-       if req.path.match(/cart/)
+      else
+      resp.write "Path Not Found"
+    end
+    if req.path.match(/cart/)
       @@cart.each do |cart|
         resp.write "#{cart}\n"
       end
       else
-      resp.write "Path Not Found"
-    end
-   
-      
+        resp.write "Path not found.Your cart is empty"
+      end
 
     resp.finish
   end
